ec65b7a7cd985fd0537c03a6d4ee568ffce22fb3
fuookami <bagawolf2014@yahoo.co.jp>
2017-04-27 23:17:48 +0800

    构建航班计划类

--- AutomaticScheduing/AutomaticScheduing/FlightPlan.cpp
+++ AutomaticScheduing/AutomaticScheduing/FlightPlan.cpp
@@ -1,1 +1,34 @@
 #include "FlightPlan.h"
+
+#include <algorithm>
+
+void FlightPlan::setFlighterNums(const unsigned int i)
+{
+	orgBunches.insert(orgBunches.begin(), i, FlightBunch());
+}
+
+std::shared_ptr<FlightPlan> FlightPlan::generateFromPlanTable(const PlanTable & t, const FlightInfoMap & infoMap)
+{
+	std::shared_ptr<FlightPlan> pNewPlan(new FlightPlan());
+	for (unsigned int i(0), j(t.size()); i != j; ++i)
+	{
+		if (!pNewPlan->bunches[t[i]].addFlight(infoMap.find(i)->second));
+			return nullptr;
+	}
+
+	for (const FlightBunch &bunch : pNewPlan->bunches)
+		pNewPlan->totalDelay += bunch.delay();
+
+	return pNewPlan;
+}
+
+const Time &FlightPlan::delay(void) const
+{
+	return totalDelay;
+}
+
+FlightPlan::FlightPlan()
+	: bunches(orgBunches), totalDelay(Time(0, 0))
+{
+
+}
--- AutomaticScheduing/AutomaticScheduing/FlightPlan.h
+++ AutomaticScheduing/AutomaticScheduing/FlightPlan.h
@@ -1,8 +1,22 @@
 #pragma once
 
 #include "FlightBunch.h"
+#include <vector>
+
+using PlanTable = std::vector<unsigned int>;
 
 class FlightPlan
 {
-	
+public:
+	static void setFlighterNums(const unsigned int i);
+	static std::shared_ptr<FlightPlan> generateFromPlanTable(const PlanTable &t, const FlightInfoMap &infoMap);
+	const Time &delay(void) const;
+
+private:
+	FlightPlan();
+
+private:
+	static std::vector<FlightBunch> orgBunches;
+	std::vector<FlightBunch> bunches;
+	Time totalDelay;
 };
